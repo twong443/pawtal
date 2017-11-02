7b142a8b1f7a04b209cf3765d147c99d2ab70659
Tiffany Wong <t.wong.443@gmail.com>
2017-11-02 14:11:25 -0400

    Fix click event listener function

--- public/scripts/pawtal.js
+++ public/scripts/pawtal.js
@@ -1,9 +1,9 @@
 // //make rows into links
 var rowUrls = document.querySelectorAll(".rowUrl");
-    
-for(var i=0; i<rowUrls.length; i++){
-    var petId = rowUrls[i].attributes[1].textContent;
-    rowUrls[i].addEventListener("click", function(){
+
+rowUrls.forEach(function(row){
+    var petId = row.attributes[1].textContent;
+    row.addEventListener("click", function(){
         window.location.assign("/patients/" + petId);
     });
-}
+});
